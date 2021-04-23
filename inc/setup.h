363f3881cd42d8ce4749eb5489f1a3abffff2a8a
274699 <48215368+274699@users.noreply.github.com>
2021-04-23 22:46:08 +0530

    Update setup.h

--- inc/setup.h
+++ inc/setup.h
@@ -1,3 +1,13 @@
+/**
+ * @file setup.h
+ * @author vishal_274699
+ * @brief header file for activity 1
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef SETUP_H_INCLUDED
 #define SETUP_H_INCLUDED
 
